7f35bb4d217ade2d84ec7f24ebbaff3a6f6ea1b0
nparker <ncpark3r@gmail.com>
2020-10-01 17:01:15 +1000

    Added option fetching and printing of help text

--- user_upload.php
+++ user_upload.php
@@ -21,6 +21,43 @@
 
 // calling --help will output the CLI help instructions
 
+// parsing CLI arguments
+$shortopts  = "";
+$shortopts .= "f:";   // Required value for csv file.
+$shortopts .= "c";    // No value.
+$shortopts .= "d";    // No value.
+$shortopts .= "u:";   // Required value for MySQL user.
+$shortopts .= "p:";   // Required value for MySQL password.
+$shortopts .= "h";    // No value.
+
+$longopts  = array(
+    "file:",          // Required value for csv file.
+    "create_table",   // No value for create table.
+    "dry_run",        // No value for dry run.
+    "help",           // No value for help.
+);
+// Get options.
+$options = getopt($shortopts, $longopts);
+
+// If help option was specified print help text and die.
+if (array_key_exists("help", $options) == true || array_key_exists("h", $options) == true) {
+    fwrite(STDOUT,
+    "This CLI accepts a CSV file as input, processes it and the contained users are added to a MySQL database table.
+Options:
+-f, --file            CSV file name for users
+-c, --create_table    Create the users table and do nothing else
+-d, --dry_run         Run the script with all functionality aside from DB actions
+-u                    MySQL username
+-p                    MySQL password
+-h, --help            Print this help text
+Example:
+
+\$ php user_upload --file 'users.csv'\
+        -u 'MySQL_username' -p 'MySQL_password' -h 'MySQL_host'\n");
+    die;
+}
+
+var_dump($options);
 
 
 
