042cf7cbe8518aed26a5f3e15823e48f654c9eee
npark3r <51395247+npark3r@users.noreply.github.com>
2020-10-02 10:21:20 +1000

    Update user_upload.php

--- user_upload.php
+++ user_upload.php
@@ -2,8 +2,8 @@
 // This script is executed from the command line and and accepts a CSV
 // file as input and processes it. The parsed file is inserted in a MySQL
 // database specified by additional arguments.
-// An example command line instruction run script is:
-//      php user_upload.php --file user.csv --dry_run
+// An example command line instruction to run script is:
+//      php user_upload.php --file users.csv --dry_run
 
 // The above example will run the script without accessing the DB
 
@@ -16,7 +16,7 @@
 
 // Another example command line instruction to run script is:
 
-//      php user_upload.php --file user.csv -u MySQL_username -p MySQL_password
+//      php user_upload.php --file users.csv -u MySQL_username -p MySQL_password
 //                          -h MySQL_host
 
 // The above example will cause the MySQL users table to be built (with no other action taken)
